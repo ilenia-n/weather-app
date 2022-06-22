5e31cbae5ef8543bb7d432f043bfbc33757e1c48
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-06-22 17:49:03 +0100

    test on netfly

--- javascript/index.js
+++ javascript/index.js
@@ -25,7 +25,7 @@ function onFormSubmit(event) {
 
 function requestDataFromAPI(requestObject) {
   let baseUrl = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "2c9e54e4fa31d7c6cf4c3825eadb1e42";
+  let apiKey = key;
   let apiUrl = `${baseUrl}?q=${requestObject}&appid=${apiKey}&units=metric`;
   if (typeof(requestObject) == "object") {
     apiUrl = `${baseUrl}?lat=${requestObject.latitude}&lon=${requestObject.longitude}&appid=${apiKey}&units=metric`;
@@ -81,7 +81,7 @@ function displayFiveDaysForecast(response) {
 }
 
 function getForecastOneCall(coordinates) {
-  let apiKey = "2c9e54e4fa31d7c6cf4c3825eadb1e42";
+  let apiKey = key;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayFiveDaysForecast);
 }
